75bb7ea6babffa4e6dd3c71670dd2a1c2e19d1e9
Sabrin16 <sabrin.salem@hotmail.se>
2025-04-19 23:58:37 +0200

    update

--- site.js
+++ site.js
@@ -1,4 +1,68 @@
-document.querySelectorAll('.form-select').forEach((select) => {
+const dropdowns = document.querySelectorAll('[data-type="dropdown"]')
+
+document.addEventListener('click', function (event) {
+  let clickedDropdown = null
+
+  dropdowns.forEach(dropdown => {
+    const targetId = dropdown.getAttribute('data-target')
+    const targetElement = document.querySelector(targetId)
+
+    if (dropdown.contains(event.target)) {
+      clickedDropdown = targetElement
+
+      document.querySelectorAll('.dropdown.dropdown-show').forEach(openDropdown => {
+        if (openDropdown !== targetElement) {
+          openDropdown.classList.remove('dropdown-show')
+        }
+      })
+
+      targetElement.classList.toggle('dropdown-show')
+    }
+  })
+
+  if (!clickedDropdown && !event.target.closest('.dropdown')) {
+    document.querySelectorAll('.dropdown.dropdown-show').forEach(openDropdown => {
+      openDropdown.classList.remove('dropdown-show')
+    })
+  }
+
+  
+
+
+  const modals = document.querySelectorAll('[data-type="modal"]')
+
+  document.addEventListener('click', function (event) {
+    let clickedDropdown = null
+  
+    modals.forEach(modal => {
+      const targetId = modal.getAttribute('data-target')
+      const targetElement = document.querySelector(targetId)
+  
+      if (modal.contains(event.target)) {
+        clickedDropdown = targetElement
+  
+        document.querySelectorAll('.dropdown.dropdown-show').forEach(openDropdown => {
+          if (openDropdown !== targetElement) {
+            openDropdown.classList.remove('dropdown-show')
+          }
+        })
+  
+        targetElement.classList.toggle('dropdown-show')
+      }
+    })
+  
+    if (!clickedDropdown && !event.target.closest('.dropdown')) {
+      document.querySelectorAll('.dropdown.dropdown-show').forEach(openDropdown => {
+        openDropdown.classList.remove('dropdown-show')
+      })
+    }
+  })
+
+
+
+
+
+document.querySelectorAll('.form-select').forEach(select => {
   const trigger = select.querySelector('.form-select-trigger')
   const triggerText = trigger.querySelector('.form-select-text')
   const options = select.querySelectorAll('.form-select-option')
@@ -15,8 +79,7 @@ document.querySelectorAll('.form-select').forEach((select) => {
 
   trigger.addEventListener('click', (e) => {
     e.stopPropagation();
-    document
-      .querySelectorAll(".form-select.open")
+    document.querySelectorAll(".form-select.open")
       .forEach((el) => el !== select && el.classList.remove('open'))
     select.classList.toggle('open')
   })
